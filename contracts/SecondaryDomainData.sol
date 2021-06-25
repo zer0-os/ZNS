c3d392a4707afe65cd72c59db6ad25cab72fc245
dreamienb <dreamienb@gmail.com>
2021-06-25 05:18:04 -0600

    - decision about dynamic token address store must be made to continue

---
+++ contracts/SecondaryDomainData.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.7.3;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+contract SecondaryDomainData {
+  struct DomainData {
+    address dynamicToken;
+  }
+}
